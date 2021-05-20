5b99caacb9ed0d39f6404dd11a8536ec50a944a3
Caio Ventura <caiovnasci@gmail.com>
2021-05-20 00:00:28 -0300

    feature: next js is now configured!

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Pinkgreen.</title>
+      </Head>
+
+      <main>
+        oi
+      </main>
+    </div>
+  );
+}
