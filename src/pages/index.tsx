2ef29944da1c1cb976ff110c06519f40c29e76f4
Caio Ventura <caiovnasci@gmail.com>
2021-10-06 17:32:09 -0300

    feat: brand carousel on home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,62 @@
-import { Carousel, CategoryCard } from '@/components/index';
-import { BASE_URL } from '@/constants/api';
-import { GetStaticProps } from 'next';
-import Head from 'next/head';
+import { Carousel, CategoryCard } from '@/components/index'
+import { BASE_URL } from '@/constants/api'
+import { GetStaticProps } from 'next'
+import Head from 'next/head'
 
-const Home = ({ categories }: {categories: Array<ICategory>}) => {
+interface HomeProps {
+  categories: Array<ICategory>
+  brands: Array<IBrand>
+}
+
+const Home = ({ categories, brands }: HomeProps) => {
   return (
     <>
       <Head>
         <title>Pinkgreen.</title>
       </Head>
       <section>
-        <h1>Categories.</h1>
-        <Carousel settings={{ variableWidth: true }}>
-          {categories.map(category => {
-            return <CategoryCard key={category.id} name={category.name} id={category.id} image={category.image} />
-          })}
-        </Carousel>
+        <div>
+          <h1>Categories.</h1>
+          <Carousel settings={{ variableWidth: true }}>
+            {categories.map((category) => {
+              return (
+                <CategoryCard
+                  key={category.id}
+                  name={category.name}
+                  id={category.id}
+                  image={category.image}
+                />
+              )
+            })}
+          </Carousel>
+        </div>
+        <div>
+          <h1>Brands.</h1>
+          <Carousel settings={{ variableWidth: true }}>
+            {brands.map((brand) => {
+              return (
+                <CategoryCard
+                  key={brand.id}
+                  name={brand.name}
+                  id={brand.id}
+                  image={
+                    'https://i.kym-cdn.com/photos/images/original/001/472/727/90a.jpg'
+                  }
+                />
+              )
+            })}
+          </Carousel>
+        </div>
       </section>
     </>
-  );
-};
-
-export const getStaticProps: GetStaticProps<{categories: Array<ICategory>}>  = async () => {
-  const res = await(await fetch(`${BASE_URL}/category`)).json();
-  return { props: { categories: res } }
+  )
 }
 
-export default Home;
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const categories = await (await fetch(`${BASE_URL}/category`)).json()
+  const brands = await (await fetch(`${BASE_URL}/brand`)).json()
+
+  return { props: { categories, brands } }
+}
+
+export default Home
